b4bc7be7df356caf847f8860d6b78b36bd8274db
andriimyk <Andrii.Mykytenko@TX-N-0780>
2022-02-09 23:28:58 +0200

    Merge branch 'classwork/lesson_4_methods'

---
+++ Homework4/Homework4/classwork-4-methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson_4_methods
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Factorial(5));
+			Console.Read();
+		}
+		static int Factorial(int x)
+		{
+			int factorial;
+			factorial = 1;
+			while (x != 0)
+			{
+				factorial *= x;
+				x--;
+			}
+			/*for (int i = x; i > 0; i--)
+			{
+				factorial *= i;
+			}*/
+			return factorial;
+		}
+	}
+}
+
